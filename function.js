025eff90a1d1d6c967ff9b06b2ef8aa49648c4f1
JosepH4unter <61758267+JosepH4unter@users.noreply.github.com>
2021-08-20 18:06:41 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,1 +1,12 @@
-
+<script>
+    $(function(){
+        $('nav.mobile i').click(function(){
+            var el = $(this).parent().find('ul');
+            if(el.is(':visible') == false){
+            el.fadeIn();
+            }else{
+                el.fadeOut();
+            }
+        })
+    })
+</script>
