60837751751836954822167b9f1687f0c5c86c87
JosepH4unter <61758267+JosepH4unter@users.noreply.github.com>
2021-08-21 17:56:52 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
     $(function(){
-        $('.mobile').click(function(){
+        $('.mobile').click(function(){//eu tirei a especificacao de ul para ir para o componente como um todo
             var el = $(this).parent().find('ul');
             if(el.is(':visible') == false){
             el.fadeIn();
